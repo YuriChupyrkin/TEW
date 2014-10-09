df11a32efe312243765ded053eb3ea0752908532
unknown <Yuri@.(none)>
2014-10-10 00:44:14 +0100

    creating data base after start web app

--- TewCloud/Global.asax.cs
+++ TewCloud/Global.asax.cs
@@ -2,6 +2,9 @@
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Domain.Entities;
+using Domain.RepositoryFactories;
+using Domain.UnitOfWork;
 using TewCloud.App_Start;
 
 namespace TewCloud
@@ -16,6 +19,10 @@ namespace TewCloud
       BundleConfig.RegisterBundles(BundleTable.Bundles);
 
       AutofacModule.RegisterAutoFac();
+
+      var repositoryFactory = DependencyResolver.Current.GetService <IRepositoryFactory>();
+      repositoryFactory.EnRuWordsRepository.AddTranslate("TEW", "TEW", "TEW", 1);
+      ((IUnitOfWork)repositoryFactory).Commit();
     }
   }
 }
