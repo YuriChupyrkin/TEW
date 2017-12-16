9486e2b65d833ea9563eac79e008709959aaecf7
YuriChupyrkin <yuri.chupyrkin@gmail.com>
2017-12-16 13:21:42 +0300

    Fix Webpack config

--- WebSite/StaticContent/webpack.config.js
+++ WebSite/StaticContent/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
       {
         test: /\.ts$/,
         enforce: 'pre',
-        loader: ['tslint-loader', 'ts-loader'],
+        loader: ['ts-loader', 'tslint-loader'],
       },
       {
         test: /\.scss$/,
